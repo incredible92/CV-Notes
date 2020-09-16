1fc360d6b7481d4bdcf5cdbe4b5b385c3ce9914b
incredible92 <sulymanabdullahi@gmail.com>
2020-09-16 19:38:10 +0100

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.scss";
+
+const SearchBox = () => (
+  <div className="SearchBox">
+    <img src="/images/Vector.png" className="img" />
+
+    <input type="text" placeholder="search" className="input" />
+  </div>
+);
+
+const TodoList = () => (
+  <div className="TodoList">
+    <span>Abdullahi Sulyman</span>
+    <span>16-sep-2020</span>
+  </div>
+);
+
+const Wrapper = () => (
+  <div className="Wrapper">
+    <h1 className="heading">CV Note</h1>
+    <SearchBox />
+    <TodoList />
+  </div>
+);
+
+const NoteWrapper = () => (
+  <div className="NoteWrapper">
+    <input type="text" ClassName="text" placeholder="Title" />
+
+    <textarea ClassName="MyTextarea" placeholder="type your note"></textarea>
+
+    <div ClassName="rectangles">
+      <img src="/images/Rectangle 4.png" alt="image" />
+      <img src="/images/Rectangle 7.svg" alt="image" />
+      <img src="/images/Rectangle 8.png" alt="image" />
+      <img src="/images/Rectangle 9.png" alt="image" />
+    </div>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper />
+      <NoteWrapper />
     </div>
   );
 }
